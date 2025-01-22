922577d035a9c25f407b1bc78961acfb90931fd4
airconditionerale <ailee.choi@rectorywolves.org>
2025-01-22 11:07:56 -0500

    For loop added

--- test.py
+++ test.py
@@ -1,1 +1,17 @@
-print("Hi")
+
+alph=("ABCDEFGHIJKLMNOPQRSTUVWXYZ")
+wrd= input("gimme a word (all CAPS)")
+key= input("gimme the key") 
+
+stream = []
+done = []
+j = 0
+
+for i in range(len(wrd)):
+	if(i>=len(key)):
+		stream.append(key[i-len(key)])
+	else:
+		stream.append(key[i])
+
+print(stream)
+
